069188b51e7c059c63c570be1123db7eeed6e983
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:16:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,12 +5,12 @@ require_relative './course.rb'
 
 class Scraper
 
-  def get_page()
+  def get_page
     Nokogiri::HTML("http://learn-co-curriculum.github.io/site-for-scraping/courses")
   end
 
-  def get_courses(html)
-
+  def get_courses
+    get_page.css(".post")
   end
 
   def print_courses
