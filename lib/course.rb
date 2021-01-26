fac6f182d2de7ee3c6a83bfaa3e24812783efae8
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:09:29 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,6 @@
 class Course
-  attr_accessor :title, :schedule, :Description
+  attr_accessor :title, :schedule, :description
+
   @@all = []
   def initialize(title, schedule, description)
     @title = title
