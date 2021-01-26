94f1b47f51af2f8c64725fcb4c29831f4dbb7315
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:08:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,19 +4,17 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
+        Course.new(course.title, course.schedule, course.description)
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
       end
     end
   end
-  
+
 end
-
-
-
