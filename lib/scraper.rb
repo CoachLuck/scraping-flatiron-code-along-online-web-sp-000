86ee078436c94a97c182a4cf7e316b166a471ad4
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:08:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,6 @@ class Scraper
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
-        Course.new(course.title, course.schedule, course.description)
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
