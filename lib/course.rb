ff471724b47188fc70e63c2c832fc7322127eb29
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 18:07:07 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,10 +1,17 @@
 class Course
   attr_reader :name, :schedule, :Description
-
+  @@all = []
   def initialize(name, schedule, description)
     @name = name
     @schedule = schedule
     @description = description
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
   
+
 end
